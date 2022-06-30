60ff7f75a422ae95140ae37c48f8623155cbd59f
mhazheer <46894117+mhazheer@users.noreply.github.com>
2022-06-30 10:51:21 +0200

    Add files via upload
    code infra terraform

---
+++ infra.tf
@@ -0,0 +1,173 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+# Subnets
+## Public
+### AZ1
+resource "aws_subnet" "subnet-public-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1a"
+  tags = {
+    Name = "${var.env}-subnet-public-1"
+  }
+}
+
+### AZ2
+resource "aws_subnet" "subnet-public-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1b"
+  tags = {
+    Name = "${var.env}-subnet-public-2"
+  }
+}
+
+### AZ3
+resource "aws_subnet" "subnet-public-3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1c"
+  tags = {
+    Name = "${var.env}-subnet-public-3"
+  }
+}
+
+## Private
+### AZ1
+resource "aws_subnet" "subnet-private-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1a"
+  tags = {
+    Name = "${var.env}-subnet-private-1"
+  }
+}
+
+### AZ2
+resource "aws_subnet" "subnet-private-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.5.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1b"
+  tags = {
+    Name = "${var.env}-subnet-private-2"
+  }
+}
+
+### AZ3
+resource "aws_subnet" "subnet-private-3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.6.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1c"
+  tags = {
+    Name = "${var.env}-subnet-private-3"
+  }
+}
+
+
+## SG Rule egress
+resource "aws_security_group_rule" "web_egress_allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_nat.id
+}
+
+## SG Rule ingress
+resource "aws_security_group_rule" "ingress_allow_private" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  security_group_id = aws_security_group.allow_nat.id
+}
+
+*/
+
+# Route Table
+## Private
+### Use Main Route Table
+resource "aws_default_route_table" "main-private" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+/*
+  route {
+    cidr_block  = "0.0.0.0/0"
+    instance_id = aws_instance.nat.id
+  }
+*/
+  tags = {
+    Name = "${var.env}-rt-main-private"
+  }
+}
+
+## Public (accès à internet)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-rt-public"
+  }
+}
+
+# Public Route Table Association
+resource "aws_route_table_association" "public-1" {
+  subnet_id      = aws_subnet.subnet-public-1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-2" {
+  subnet_id      = aws_subnet.subnet-public-2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-3" {
+  subnet_id      = aws_subnet.subnet-public-3.id
+  route_table_id = aws_route_table.public.id
+}
+terraform {
+  backend "s3" {
+ 
+  }
+}
