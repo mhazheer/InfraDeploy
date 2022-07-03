8309182a43c973fff269cd43e65f12b7a4a0e023
mhazheer <46894117+mhazheer@users.noreply.github.com>
2022-07-03 12:40:41 +0200

    ajout s3

--- infra.tf
+++ infra.tf
@@ -189,3 +189,7 @@ resource "aws_route_table_association" "public-3" {
   subnet_id      = aws_subnet.subnet-public-3.id
   route_table_id = aws_route_table.public.id
 }
+terraform {
+  backend "s3"{
+  }
+}
